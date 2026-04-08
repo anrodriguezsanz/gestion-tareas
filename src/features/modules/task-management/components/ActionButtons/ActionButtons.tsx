19c16696ced0a7b980b2229cc3b14d3c1f13377b
Andrea Rodríguez Sanz <Andrea.rodriguez@aquiles.local>
2026-04-08 12:44:49 +0200

    feat: confirm modal added on ActionButtons.tsx

--- src/features/modules/task-management/components/ActionButtons/ActionButtons.tsx
+++ src/features/modules/task-management/components/ActionButtons/ActionButtons.tsx
@@ -1,4 +1,4 @@
-import { Space, Button } from 'antd';
+import { Space, Button, Modal } from 'antd';
 import { Link } from 'react-router-dom';
 import type { Task } from '../../models/Task';
 import { DETAILS_PATH, COMPLETED_STATUS, PENDING_STATUS } from '../../../../../core/constants';
@@ -9,16 +9,29 @@ interface ActionButtonsProps {
     onDelete: (id: number) => void;
 }
 
-export const ActionButtons = ({ record, onToggleStatus, onDelete }: ActionButtonsProps) => (
-    <Space size='small'>
-        <Button type='primary' onClick={() => onToggleStatus(record.id)}>
-            Marcar como {record.status === PENDING_STATUS ? COMPLETED_STATUS.toLowerCase() : PENDING_STATUS.toLowerCase()}
-        </Button>
-        <Link to={`${DETAILS_PATH}${record.id}`}>
-            <Button>Ver detalles</Button>
-        </Link>
-        <Button onClick={() => onDelete(record.id)}>
-            Eliminar
-        </Button>
-    </Space>
-)
+export const ActionButtons = ({ record, onToggleStatus, onDelete }: ActionButtonsProps) => {
+    
+    const handleDelete = () => {
+        Modal.confirm({
+            title: '¿Estás seguro de eliminar esta tarea?',
+            content: `Se eliminará la tarea: "${record.title}"`,
+            okText: 'Eliminar',
+            cancelText: 'Cancelar',
+            onOk: () => onDelete(record.id),
+        });
+    };
+
+    return (
+        <Space size='small'>
+            <Button type='primary' onClick={() => onToggleStatus(record.id)}>
+                Marcar como {record.status === PENDING_STATUS ? COMPLETED_STATUS.toLowerCase() : PENDING_STATUS.toLowerCase()}
+            </Button>
+            <Link to={`${DETAILS_PATH}${record.id}`}>
+                <Button>Ver detalles</Button>
+            </Link>
+            <Button onClick={handleDelete}>
+                Eliminar
+            </Button>
+        </Space>
+    );
+}
